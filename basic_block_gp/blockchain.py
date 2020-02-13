0d5648fb5760664aa1fe7f0d8ae04aedaf522eb0
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2020-02-12 19:31:48 -0500

    Wrote most of the logic to switch mining over to the client file. Have to debug

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -13,20 +13,20 @@ class Blockchain(object):
         self.new_block(previous_hash="I'm a teapot.", proof=100)
 
     def new_block(self, proof, previous_hash=None):
-#         """
-#         Create a new Block in the Blockchain
-# ​
-#         A block should have:
-#         * Index
-#         * Timestamp
-#         * List of current transactions
-#         * The proof used to mine this block
-#         * The hash of the previous block
-# ​
-#         :param proof: <int> The proof given by the Proof of Work algorithm
-#         :param previous_hash: (Optional) <str> Hash of previous Block
-#         :return: <dict> New Block
-#         """
+        """
+        Create a new Block in the Blockchain
+​
+        A block should have:
+        * Index
+        * Timestamp
+        * List of current transactions
+        * The proof used to mine this block
+        * The hash of the previous block
+​
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
 
         block = {
             'index': len(self.chain) + 1,
@@ -44,12 +44,12 @@ class Blockchain(object):
         return block
 
     def hash(self, block):
-#         """
-#         Creates a SHA-256 hash of a Block
-# ​
-#         :param block": <dict> Block
-#         "return": <str>
-#         """
+        """
+        Creates a SHA-256 hash of a Block
+​
+        :param block": <dict> Block
+        "return": <str>
+        """
 
         # Use json.dumps to convert json into a string
         # Use hashlib.sha256 to create a hash
@@ -81,13 +81,13 @@ class Blockchain(object):
         return self.chain[-1]
 
     def proof_of_work(self, block):
-        # """
-        # Simple Proof of Work Algorithm
-        # Stringify the block and look for a proof.
-        # Loop through possibilities, checking each one against `valid_proof`
-        # in an effort to find a number that is a valid proof
-        # :return: A valid proof for the provided block
-        # """
+        """
+        Simple Proof of Work Algorithm
+        Stringify the block and look for a proof.
+        Loop through possibilities, checking each one against `valid_proof`
+        in an effort to find a number that is a valid proof
+        :return: A valid proof for the provided block
+        """
         
         block_string = json.dumps(block, sort_keys=True)
         proof = 0
@@ -98,16 +98,16 @@ class Blockchain(object):
 
     @staticmethod
     def valid_proof(block_string, proof):
-        # """
-        # Validates the Proof:  Does hash(block_string, proof) contain 3
-        # leading zeroes?  Return true if the proof is valid
-        # :param block_string: <string> The stringified block to use to
-        # check in combination with `proof`
-        # :param proof: <int?> The value that when combined with the
-        # stringified previous block results in a hash that has the
-        # correct number of leading zeroes.
-        # :return: True if the resulting hash is a valid proof, False otherwise
-        # """
+        """
+        Validates the Proof:  Does hash(block_string, proof) contain 3
+        leading zeroes?  Return true if the proof is valid
+        :param block_string: <string> The stringified block to use to
+        check in combination with `proof`
+        :param proof: <int?> The value that when combined with the
+        stringified previous block results in a hash that has the
+        correct number of leading zeroes.
+        :return: True if the resulting hash is a valid proof, False otherwise
+        """
         guess = f'{block_string}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
 
--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -13,7 +13,13 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    print("Searching for a valid proof...")
+    block_string = json.dumps(block, sort_keys=True)
+    proof = 0
+    while valid_proof(block_string, proof) is False:
+        proof += 1
+    print("Finished! Valid proof is: ", proof)
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,7 +33,10 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    return guess_hash[:6] == "000000"
 
 
 if __name__ == '__main__':
@@ -49,6 +58,7 @@ if __name__ == '__main__':
         # Handle non-json response
         try:
             data = r.json()
+            print(data)
         except ValueError:
             print("Error:  Non-json response")
             print("Response returned:")
@@ -56,7 +66,7 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+        new_proof = proof_of_work(data)
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
